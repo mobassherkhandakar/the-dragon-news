b7594705382f39b057971b2442e2fe806bf0b563
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-04-24 10:43:34 +0600

    right side done

--- src/pages/Shered/RightNav/RightNav.jsx
+++ src/pages/Shered/RightNav/RightNav.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const RightNav = () => {
   return (
-    <div>
-      <h1>this is a right side nav</h1>
-    </div>
+    <>
+      <h4>Login with</h4>
+      <div>
+        <Button className="mb-2 text-center w-100" variant="outline-primary">
+          <FaGoogle /> Login with Google
+        </Button>
+        <Button className="text-center w-100" variant="outline-primary">
+          <FaGithub /> Login with Github
+        </Button>
+      </div>
+      <div>
+        <h4>Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item className="py-4"><FaFacebook/> Facebook</ListGroup.Item>
+          <ListGroup.Item className="py-4"><FaTwitter/> Twitter </ListGroup.Item>
+          <ListGroup.Item className="py-4"><FaInstagram/> Instragram</ListGroup.Item>
+        </ListGroup>
+      </div>
+    </>
   );
 };
 
